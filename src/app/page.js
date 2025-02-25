228ccefc8e88fa912cbb90de6621c276c3adc5f5
ValeriiViachalo <valeraviachalo@gmail.com>
2025-02-25 21:43:54 +0200

    Refactor Home component and create new page1 component with data fetching

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,7 @@
-import HomePage from "@/components/HomePage/HomePage";
-import { getFetchData } from "@/lib/helpers/DataFetch";
-import { URL_HOME } from "@/lib/helpers/DataUrls";
-import { generatePagesMetadata } from "@/lib/helpers/generatePagesMetadata";
-import { useLanguageContent } from "@/lib/helpers/useLanguageContent";
-import { DataProvider } from "@/lib/providers/DataProvider/DataProvider";
+import React from 'react'
 
-export const generateMetadata = async () => generatePagesMetadata(URL_HOME);
-
-export default async function Home() {
-  const preparedData = await getFetchData(URL_HOME);
-  const data = useLanguageContent(preparedData, "en");
-  
+export default function page() {
   return (
-    <DataProvider data={data}>
-      <HomePage />
-    </DataProvider>
-  );
+    <div>page</div>
+  )
 }
