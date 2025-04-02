e2bd6f01aee159ae7e349fc9744d9891989c0778
ValeriiViachalo <valeraviachalo@gmail.com>
2025-04-02 13:51:09 +0300

    asassaa

--- src/app/(en)/sell/page.js
+++ src/app/(en)/sell/page.js
@@ -1,19 +1,28 @@
-import HomePage from "@/components/HomePage/HomePage";
-import { getFetchData } from "@/lib/helpers/DataFetch";
-import { URL_SELL_PAGE } from "@/lib/helpers/DataUrls";
-import { generatePagesMetadata } from "@/lib/helpers/generatePagesMetadata";
-import { useLanguageContent } from "@/lib/helpers/useLanguageContent";
-import { DataProvider } from "@/lib/providers/DataProvider/DataProvider";
+// import HomePage from "@/components/HomePage/HomePage";
+// import { getFetchData } from "@/lib/helpers/DataFetch";
+// import { URL_SELL_PAGE } from "@/lib/helpers/DataUrls";
+// import { generatePagesMetadata } from "@/lib/helpers/generatePagesMetadata";
+// import { useLanguageContent } from "@/lib/helpers/useLanguageContent";
+// import { DataProvider } from "@/lib/providers/DataProvider/DataProvider";
 
-export const generateMetadata = async () => generatePagesMetadata(URL_SELL_PAGE);
+// export const generateMetadata = async () => generatePagesMetadata(URL_SELL_PAGE);
 
-export default async function Home() {
-  const preparedData = await getFetchData(URL_SELL_PAGE);
-  const data = useLanguageContent(preparedData, "en");
+// export default async function Home() {
+//   const preparedData = await getFetchData(URL_SELL_PAGE);
+//   const data = useLanguageContent(preparedData, "en");
   
+//   return (
+//     <DataProvider data={data}>
+//       <HomePage lang="" type="sell"/>
+//     </DataProvider>
+//   );
+// }
+export default async function Home() {
   return (
-    <DataProvider data={data}>
-      <HomePage lang="" type="sell"/>
-    </DataProvider>
-  );
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the home page!</p>
+      <p>This is a placeholder for the actual content.</p>
+    </div>
+  )
 }
